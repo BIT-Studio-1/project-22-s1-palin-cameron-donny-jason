4e3b447555be2b78b2af73c6190a5897e9f5c2a1
Palin Wiseman <wisepb1@student.op.ac.nz>
2022-05-23 13:22:01 +1200

    Added a movement system

--- Program.cs
+++ Program.cs
@@ -6,7 +6,66 @@ namespace Group_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RoomA();
+        }
+        public static void RoomA()
+        {
+            bool fail;
+            string temp;
+            do
+            {
+                Console.WriteLine("You are standing in a themed place. You are facing north. \nThere is a door to your left and a passage to your right. Where do you go?");
+                temp = Console.ReadLine().ToLower(); //Gets the command
+                fail = false; //Sets the do while loop to end unless this is changed
+                switch (temp)
+                {
+                    case "left":
+                    case "west":
+                    case "door":
+                    case "open door":
+                    case "go through door": //Listing cases. Can change depending on the theme.
+                        RoomB();            //Go to room B
+                        break;
+                    case "right":
+                    case "east":
+                    case "passage":
+                    case "go through passage": //Ditto
+                        RoomC();
+                        break;
+                    case "help":                //If they ask for help
+                        Help();                 //Sending them to the help menu
+                        fail = true;            //Making sure it loops again
+                        break;
+                    default:
+                        Console.WriteLine("I can't understand that input. Please try again or type help for tips."); //Default if they don't put anything userful in
+                        fail = true;            //Making sure it loops again
+                        break;
+                }
+            } while (fail == true);             //Looping again if needed
+        }
+        public static void RoomB()
+        {
+            Console.WriteLine("You are in room B");
+            Pause();
+            //This is just here to ensure that you have made it to room b. Fill out later
+        }
+        public static void RoomC()
+        {
+            Console.WriteLine("You are in room C");
+            Pause();
+            //This is just here to ensure that you have made it to room c. Fill out later
+        }
+        public static void Help()
+        {
+            Console.WriteLine("You are in the help area");
+            Console.WriteLine("Press any key to continue");
+            Console.Clear();
+            Pause();
+            //Have some general guidance for what stuff the program is looking for
+        }
+        public static void Pause() //Because I'm lazy and Pause(); is faster to type and doesn't cause issues if I use it multiple times.
+        {
+            _ = Console.ReadKey();
         }
     }
 }
